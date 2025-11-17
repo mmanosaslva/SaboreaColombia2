47562c2057578d0580419ca0f9353a2c90f9cf60
Laurahernandez-29 <law-026@hotmail.com>
2025-11-16 22:35:56 -0500

    Delete back/src/usuarios/entities/updateusuario.dto.ts

--- back/src/usuarios/entities/updateusuario.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUsuarioDto } from './createusuario.dto';
-
-export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {}
